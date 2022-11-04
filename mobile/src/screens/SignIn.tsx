1b50bb03b594d6664b3c99bbee9eee639c6e7ade
Caroline Vieira <carolinesanvieira@gmail.com>
2022-11-04 17:25:00 +0100

    SignIn with google screen

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,12 +1,24 @@
-import { Center } from "native-base";
+import { Center, Icon, Text } from "native-base";
 import Logo from '../assets/logo.svg'
 import { Button } from "../components/Button";
+import { Fontisto } from '@expo/vector-icons'
+
 
 export function SignIn(){
     return(
       <Center flex={1} bgColor="gray.900">
         <Logo />
-        <Button title="Google Login"/>
-    </Center>
+        <Button 
+          type='SECONDARY'
+          title="SignIn with Google"
+          leftIcon={<Icon as={Fontisto} name="google" color="white" size="md"/>}
+          mt={12}
+          />
+          <Text
+            color="white" textAlign="center" mt={4}
+          >
+           We do not use any information other than {'\n'} your email to create your account.
+          </Text>
+      </Center>
     )
 }
