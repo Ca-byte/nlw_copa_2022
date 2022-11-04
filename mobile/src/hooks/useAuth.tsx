fbe6d9d4a3375a280f6aec4b36aacd5433163d55
Caroline Vieira <carolinesanvieira@gmail.com>
2022-11-04 23:12:19 +0100

    Google authentication

--- mobile/src/hooks/useAuth.tsx
+++ mobile/src/hooks/useAuth.tsx
@@ -1,6 +1,7 @@
 import { useContext} from 'react'
 
 
+
 import { AuthContext, AuthContextDataProps } from '../contexts/AuthContext';
 
 export function useAuth(): AuthContextDataProps {
--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -7,23 +7,22 @@ import Logo from '../assets/logo.svg'
 
 export function SignIn(){
   const { signIn, user} = useAuth()
-
-  console.log(user)
-    return(
-      <Center flex={1} bgColor="gray.900">
-        <Logo />
-        <Button 
-          type='SECONDARY'
-          title="SignIn with Google"
-          leftIcon={<Icon as={Fontisto} name="google" color="white" size="md"/>}
-          mt={12}
-          onPress={signIn}
-          />
-          <Text
-            color="white" textAlign="center" mt={4}
-          >
-           We do not use any information other than {'\n'} your email to create your account.
-          </Text>
-      </Center>
-    )
+  
+  return(
+    <Center flex={1} bgColor="gray.900">
+      <Logo />
+      <Button 
+        type='SECONDARY'
+        title="SignIn with Google"
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md"/>}
+        mt={12}
+        onPress={signIn}
+        />
+        <Text
+          color="white" textAlign="center" mt={4}
+        >
+          We do not use any information other than {'\n'} your email to create your account.
+        </Text>
+    </Center>
+  )
 }
